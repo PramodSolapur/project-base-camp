ab0c7ac6e5715ba27df19058ad8a178a47a28773
PramodSolapur <psolapur5@gmail.com>
2025-11-11 12:31:08 +0530

    add cors configurations

--- src/app.js
+++ src/app.js
@@ -8,6 +8,16 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+// cors configurations
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true, // to access cookies
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+  }),
+);
+
 app.get("/", (req, res) => {
   res.json({ status: "Success" });
 });
